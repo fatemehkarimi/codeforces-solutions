664759b84395b81436df04c237cfd8053260d0c1
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2024-06-12 22:30:21 +0330

    codeforces 50A- Domino piling

---
+++ codeforces-50A.go
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/50/A
+// A. Domino piling
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i, j int
+	fmt.Scan(&i, &j)
+	fmt.Println((i * j) / 2)
+}
+
